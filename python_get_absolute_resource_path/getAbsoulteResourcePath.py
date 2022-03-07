64aaa8673aca4aa19feeb2bd6a0572a735c710c8
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-08 07:06:39 +0900

    Change variable name

--- python_get_absolute_resource_path/getAbsoulteResourcePath.py
+++ python_get_absolute_resource_path/getAbsoulteResourcePath.py
@@ -5,12 +5,12 @@ import posixpath
 
 def get_absolute_resource_path(res: str):
     stack_lst = inspect.stack()
-    ico_frame_idx = 0
+    res_frame_idx = 0
     for i in range(len(stack_lst)):
         context = stack_lst[i].code_context[0]
         if context.find(res) == -1:
             pass
         else:
-            ico_frame_idx = i
-    caller_path = os.path.dirname(stack_lst[ico_frame_idx].filename)
+            res_frame_idx = i
+    caller_path = os.path.dirname(stack_lst[res_frame_idx].filename)
     return os.path.join(caller_path, res).replace(os.path.sep, posixpath.sep)
