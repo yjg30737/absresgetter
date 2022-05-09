9db5314072ae48d2f3de9534b917c21c5a7f4a4e
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-09 18:35:11 +0900

    Update setup.py for pypi description

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='python-get-absolute-resource-path',
@@ -8,5 +16,7 @@ setup(
     license='MIT',
     packages=find_packages(),
     description='Get absolute resource path of exterior package',
-    url='https://github.com/yjg30737/python-get-absolute-resource-path.git'
+    url='https://github.com/yjg30737/python-get-absolute-resource-path.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description
 )
